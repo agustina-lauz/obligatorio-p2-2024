f8fefd63ffaba64170ca0f07405ef08f55e9467c
matiassenatore <msenatore@correo.um.edu.uy>
2024-06-16 20:36:19 -0300

    funciones canciones global y top 5

--- src/main/java/uy/edu/um/entities/SpotifyApp.java
+++ src/main/java/uy/edu/um/entities/SpotifyApp.java
@@ -68,5 +68,58 @@ public class SpotifyApp {
         topCanciones.forEach(cancion -> System.out.println("Posición: " + cancion.getDailyRank() +
                 " - Nombre: " + cancion.getNombre() + " - Artista: " + cancion.getArtista() ));
 
+
+    }
+
+    public static Map<String, Cancion> getCancionesGlobal(String date) {
+        Map<String, Cancion> canciones = new HashMap<>();
+        CSVFormat format = CSVFormat.DEFAULT
+                .withFirstRecordAsHeader()
+                .withIgnoreSurroundingSpaces() // Ignora espacios en blanco alrededor de los delimitadores
+                .withEscape('\\') // Especifica un carácter de escape
+                .withQuote('"') // Define el carácter de cita
+                .withDelimiter(','); // Especifica el delimitador
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile));
+             CSVParser csvParser = new CSVParser(br, format)) {
+            for (CSVRecord record : csvParser) {
+                String fecha = record.get("snapshot_date");
+                String recordCountry = record.get("country");
+                if ((fecha.equals(date.trim()) && recordCountry.isEmpty()) || recordCountry.equals(" ")) {
+                    Cancion cancion = new Cancion(
+                            record.get("spotify_id"),
+                            record.get("name"),
+                            record.get("artists"),
+                            Integer.parseInt(record.get("daily_rank")),
+                            record.get("country"),
+                            record.get("snapshot_date"),
+                            Double.parseDouble(record.get("tempo"))
+                    );
+                    canciones.put(cancion.getId(), cancion);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return canciones;
+    }
+    public static void top5CancionesGlobal(String date) {
+
+        Map<String, Cancion> cancionesMap = getCancionesGlobal(date);
+        Comparator<Cancion> comparator = Comparator.comparingInt(Cancion::getDailyRank).reversed();
+        MyHeap<Cancion> cancionesHeap = new MyHeap<>(comparator);
+        for (Cancion cancion : cancionesMap.values()) {
+            cancionesHeap.insert(cancion);
+        }
+        List<Cancion> topCanciones = cancionesHeap.extractAllSorted();
+        Collections.reverse(topCanciones);
+        System.out.println("Imprimiendo top 5 de canciones global en la fecha " + date);
+        topCanciones.stream()
+                .limit(5)  // Limita a 5 elementos
+                .forEach(cancion -> System.out.println("Posición: " + cancion.getDailyRank() +
+                        " - Nombre: " + cancion.getNombre() +
+                        " - Artista: " + cancion.getArtista()));
+
     }
 }
