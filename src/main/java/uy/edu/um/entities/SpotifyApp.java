87e8b66abc232994198d90ce658195863e492716
Agustina Lauz <agustinalauz81@gmail.com>
2024-06-23 17:30:02 -0300

    Country mapper
    Se corrige validación de países

--- src/main/java/uy/edu/um/entities/SpotifyApp.java
+++ src/main/java/uy/edu/um/entities/SpotifyApp.java
@@ -50,8 +50,10 @@ public class SpotifyApp {
 
     public static void top10CancionesPais(String date, String country) {
 
+        String countryCode = getCountryCode(country.trim());;
+
         // Obtener canciones filtradas por fecha y país
-        Map<String, Cancion> cancionesMap = getCancionesPais(date, country);
+        Map<String, Cancion> cancionesMap = getCancionesPais(date, countryCode);
         // Definir un comparador para el heap basado en daily_rank
         Comparator<Cancion> comparator = Comparator.comparingInt(Cancion::getDailyRank).reversed();
         // Crear el heap
@@ -235,6 +237,187 @@ public class SpotifyApp {
         System.out.println("Número de canciones con un tempo entre " + minTempo + " y " + maxTempo +
                 " desde " + fechaInicio + " hasta " + fechaFin + ": " + count);
         System.out.println("------------------------------------------------------------");
+    }
+
+    public static Map<String, String> countriesMap = new HashMap<>();
+
+        static {
+            countriesMap.put("uruguay", "UY");
+            countriesMap.put("argentina", "AR");
+            countriesMap.put("brasil", "BR");
+            countriesMap.put("chile", "CL");
+            countriesMap.put("paraguay", "PY");
+            countriesMap.put("bolivia", "BO");
+            countriesMap.put("peru", "PE");
+            countriesMap.put("ecuador", "EC");
+            countriesMap.put("colombia", "CO");
+            countriesMap.put("venezuela", "VE");
+            countriesMap.put("panama", "PA");
+            countriesMap.put("costa rica", "CR");
+            countriesMap.put("nicaragua", "NI");
+            countriesMap.put("honduras", "HN");
+            countriesMap.put("el salvador", "SV");
+            countriesMap.put("guatemala", "GT");
+            countriesMap.put("mexico", "MX");
+            countriesMap.put("estados unidos", "US");
+            countriesMap.put("canada", "CA");
+            countriesMap.put("reino unido", "GB");
+            countriesMap.put("españa", "ES");
+            countriesMap.put("francia", "FR");
+            countriesMap.put("alemania", "DE");
+            countriesMap.put("italia", "IT");
+            countriesMap.put("portugal", "PT");
+            countriesMap.put("suecia", "SE");
+            countriesMap.put("noruega", "NO");
+            countriesMap.put("dinamarca", "DK");
+            countriesMap.put("finlandia", "FI");
+            countriesMap.put("islandia", "IS");
+            countriesMap.put("australia", "AU");
+            countriesMap.put("nueva zelanda", "NZ");
+            countriesMap.put("japon", "JP");
+            countriesMap.put("corea del sur", "KR");
+            countriesMap.put("china", "CN");
+            countriesMap.put("india", "IN");
+            countriesMap.put("rusia", "RU");
+            countriesMap.put("sudafrica", "ZA");
+            countriesMap.put("nigeria", "NG");
+            countriesMap.put("egipto", "EG");
+            countriesMap.put("turquia", "TR");
+            countriesMap.put("grecia", "GR");
+            countriesMap.put("emiratos arabes unidos", "AE");
+            countriesMap.put("arabia saudita", "SA");
+            countriesMap.put("vietnam", "VN");
+            countriesMap.put("ucrania", "UA");
+            countriesMap.put("taiwan", "TW");
+            countriesMap.put("tailandia", "TH");
+            countriesMap.put("eslovaquia", "SK");
+            countriesMap.put("singapur", "SG");
+            countriesMap.put("rumania", "RO");
+            countriesMap.put("polonia", "PL");
+            countriesMap.put("pakistan", "PK");
+            countriesMap.put("filipinas", "PH");
+            countriesMap.put("marruecos", "MA");
+            countriesMap.put("letonia", "LV");
+            countriesMap.put("luxemburgo", "LU");
+            countriesMap.put("lituania", "LT");
+            countriesMap.put("kazajistan", "KZ");
+            countriesMap.put("hong kong", "HK");
+            countriesMap.put("hungria", "HU");
+            countriesMap.put("israel", "IL");
+            countriesMap.put("irlanda", "IE");
+            countriesMap.put("indonesia", "ID");
+            countriesMap.put("belgica", "BE");
+            countriesMap.put("bulgaria", "BG");
+            countriesMap.put("suiza", "CH");
+            countriesMap.put("chequia", "CZ");
+            countriesMap.put("republica dominicana", "DO");
+            countriesMap.put("estonia", "EE");
+            countriesMap.put("croacia", "HR");
+            countriesMap.put("chipre", "CY");
+            countriesMap.put("bielorrusia", "BY");
+            countriesMap.put("afganistán", "AF");
+            countriesMap.put("albania", "AL");
+            countriesMap.put("argelia", "DZ");
+            countriesMap.put("andorra", "AD");
+            countriesMap.put("angola", "AO");
+            countriesMap.put("antigua y barbuda", "AG");
+            countriesMap.put("armenia", "AM");
+            countriesMap.put("azerbaiyán", "AZ");
+            countriesMap.put("bahamas", "BS");
+            countriesMap.put("baréin", "BH");
+            countriesMap.put("bangladés", "BD");
+            countriesMap.put("barbados", "BB");
+            countriesMap.put("benín", "BJ");
+            countriesMap.put("bután", "BT");
+            countriesMap.put("botsuana", "BW");
+            countriesMap.put("brunéi", "BN");
+            countriesMap.put("burkina faso", "BF");
+            countriesMap.put("burundi", "BI");
+            countriesMap.put("camboya", "KH");
+            countriesMap.put("camerún", "CM");
+            countriesMap.put("cabo verde", "CV");
+            countriesMap.put("república centroafricana", "CF");
+            countriesMap.put("chad", "TD");
+            countriesMap.put("comoras", "KM");
+            countriesMap.put("congo", "CG");
+            countriesMap.put("república democrática del congo", "CD");
+            countriesMap.put("yibuti", "DJ");
+            countriesMap.put("dominica", "DM");
+            countriesMap.put("timor oriental", "TL");
+            countriesMap.put("guinea ecuatorial", "GQ");
+            countriesMap.put("eritrea", "ER");
+            countriesMap.put("etiopía", "ET");
+            countriesMap.put("fiyi", "FJ");
+            countriesMap.put("gabón", "GA");
+            countriesMap.put("gambia", "GM");
+            countriesMap.put("georgia", "GE");
+            countriesMap.put("ghana", "GH");
+            countriesMap.put("granada", "GD");
+            countriesMap.put("guinea", "GN");
+            countriesMap.put("guyana", "GY");
+            countriesMap.put("haití", "HT");
+            countriesMap.put("jamaica", "JM");
+            countriesMap.put("jordania", "JO");
+            countriesMap.put("kenia", "KE");
+            countriesMap.put("kiribati", "KI");
+            countriesMap.put("kuwait", "KW");
+            countriesMap.put("kirguistán", "KG");
+            countriesMap.put("laos", "LA");
+            countriesMap.put("líbano", "LB");
+            countriesMap.put("lesoto", "LS");
+            countriesMap.put("liberia", "LR");
+            countriesMap.put("libia", "LY");
+            countriesMap.put("malasia", "MY");
+            countriesMap.put("maldivas", "MV");
+            countriesMap.put("mali", "ML");
+            countriesMap.put("malta", "MT");
+            countriesMap.put("islas marshall", "MH");
+            countriesMap.put("mauritania", "MR");
+            countriesMap.put("micronesia", "FM");
+            countriesMap.put("moldavia", "MD");
+            countriesMap.put("mónaco", "MC");
+            countriesMap.put("mongolia", "MN");
+            countriesMap.put("montenegro", "ME");
+            countriesMap.put("mozambique", "MZ");
+            countriesMap.put("birmania", "MM");
+            countriesMap.put("namibia", "NA");
+            countriesMap.put("nauru", "NR");
+            countriesMap.put("nepal", "NP");
+            countriesMap.put("níger", "NE");
+            countriesMap.put("omán", "OM");
+            countriesMap.put("palaos", "PW");
+            countriesMap.put("paraguay", "PY");
+            countriesMap.put("qatar", "QA");
+            countriesMap.put("ruanda", "RW");
+            countriesMap.put("senegal", "SN");
+            countriesMap.put("seychelles", "SC");
+            countriesMap.put("sierra leona", "SL");
+            countriesMap.put("eslovenia", "SI");
+            countriesMap.put("islas salomón", "SB");
+            countriesMap.put("somalia", "SO");
+            countriesMap.put("sudán", "SD");
+            countriesMap.put("sudán del sur", "SS");
+            countriesMap.put("surinam", "SR");
+            countriesMap.put("esuatini", "SZ");
+            countriesMap.put("siria", "SY");
+            countriesMap.put("tayikistán", "TJ");
+            countriesMap.put("tanzania", "TZ");
+            countriesMap.put("tonga", "TO");
+            countriesMap.put("trinidad y tobago", "TT");
+            countriesMap.put("turkmenistán", "TM");
+            countriesMap.put("tuvalu", "TV");
+            countriesMap.put("uganda", "UG");
+            countriesMap.put("uzbekistán", "UZ");
+            countriesMap.put("vanuatu", "VU");
+            countriesMap.put("ciudad del vaticano", "VA");
+            countriesMap.put("yemen", "YE");
+            countriesMap.put("zambia", "ZM");
+            countriesMap.put("zimbabue", "ZW");
+
+        }
+
+    public static String getCountryCode(String countryName) {
+        return countriesMap.getOrDefault(countryName.trim().toLowerCase(), countryName);
     }
 
 }
